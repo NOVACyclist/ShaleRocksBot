e2ba6e1dca50420a5ad2696503ce534c8f33cefb
Egrets R. Herons <egretsareherons@gmail.com>
2013-09-11 15:51:25 -0700

    Herald now functional. BETA! lol

--- plugins/Welcome.pm
+++ plugins/Welcome.pm
@@ -36,10 +36,21 @@ sub getOutput {
 
 	## Join Event
 	if ($irc_event eq 'irc_join'){
-		$self->returnType("action");
-		if ($self->s("welcome_all") eq 'yes'){
-			return ("welcomes $nick to the room");
+	
+		my @wchannels = split (/ /, $self->s('herald_channels'));
+		if ($self->{channel} ~~ @wchannels){
+			my ($greeting, $type) = $self->getGreeting();
+
+			if ($type eq 'action'){
+				$self->returnType("action");
+
+			}elsif ($type eq 'command'){
+				$self->returnType("runBotCommand");
+			}
+
+			return $greeting;
 		}
+
 		return;
 	}
 
@@ -49,10 +60,114 @@ sub getOutput {
 	}
 
 	if ($cmd eq 'herald'){
-		return "soon";
+
+		my $hnick = $self->hasFlagValue("nick");
+		my $hchannel = $self->hasFlagValue("channel") || $self->{channel};
+		my $hmessage = $self->hasFlagValue("message");
+
+		my $c = $self->getCollection(__PACKAGE__, 'herald');
+		my @records;
+
+		if ($self->hasFlag("add")){
+			return "You have to use the -nick=<nick> flag." if (!$hnick);
+			return "You have to use the -message=\"<message>\" flag." if (!$hmessage);
+
+			my $type = 'text';
+			$type = 'action' if ($self->hasFlag("action"));
+			$type = 'command' if ($self->hasFlag("command"));
+			
+			#@records = $c->matchRecords({val1=>$hchannel, val3=>$hnick});
+
+			#if (@records){
+			#	$c->delete($records[0]->{row_id});
+			#}
+
+			# Channel | type | nick | message | set by nick
+			$c->add($hchannel, $type, $hnick, $hmessage, $self->accountNick());
+			return "Added welcome message \"$hmessage\" for $hnick in $hchannel.";
+		}
+
+
+		if ($self->hasFlag("show")){
+
+			if ($hnick){
+				@records = $c->matchRecords({val1=>$hchannel, val3=>$hnick});
+				return "$hnick has no welcome messages in $hchannel" if (!@records);
+
+				foreach my $rec (@records){
+					$self->addToList("[".$rec->{display_id}."] ".$rec->{val4}.GREEN." as \"$rec->{val2}\", set by $rec->{val5}".NORMAL, $self->BULLET);
+				}
+				return ("Welcome messages for $hnick in $hchannel: " . $self->getList());
+	
+
+			}else{
+				@records = $c->matchRecords({val1=>$hchannel});
+				return "No users have herald messages in $hchannel." if (!@records);
+				my %list;
+				foreach my $rec (@records){
+					$list{$rec->{val3}}++;
+				}
+
+				foreach my $k (sort keys %list){
+					$self->addToList("$k ($list{$k})", $self->BULLET);
+				}
+				return "Users with herald messages in $hchannel: " . $self->getList();
+			}
+
+
+		}
+
+
+		if ($self->hasFlag("delete")){
+			my $id = $self->hasFlagValue("id");	
+			return "-id=<#> flag is required." if (!$id);
+
+			@records = $c->matchRecords({display_id=>$id});
+			return "Can't find that record. (#$id)" if (!@records);
+
+			my $can_delete = 0;
+
+			if ( $self->hasPermission($records[0]->{val3})){
+				$can_delete = 1;
+			}
+
+			if ($self->hasPermission($records[0]->{val5})){
+				$can_delete = 1;
+			}
+		
+			if (!$can_delete){
+				return "You don't have permission to delete that message. Messages can only be deleted by the nick who created them and the target nick.";
+			}
+
+			$c->delete($records[0]->{row_id});
+			return "Deleted welcome message #" . $id;
+		}
+
+#		if ($options){
+#			my @records= $c->matchRecords({val1=>$ch, val3=>$options});
+#			return "$options has no welcome message set in $ch" if (!@records);
+#			return "$options welcome message for $ch: $records[0]->{val4} (type is $records[0]->{val2}, set by $records[0]->{val5})";
+#		}
+
+		return $self->help($cmd);
 	}
 }
 
+sub getGreeting{
+	my $self = shift;
+	my $c = $self->getCollection(__PACKAGE__, 'herald');
+	print "matching $self->{channel} and " . $self->accountNick() . "\n";
+	my @records = $c->matchRecords({val1=>$self->{channel}, val3=>$self->accountNick()});
+	my $rec;
+	if (@records){
+		$rec = @records[int(rand(@records))];
+		return ($rec->{val4}, $rec->{val2});
+	}else{
+		return ("welcomes $self->{nick} to the room.", "action");
+	}
+
+	# Channel | type | nick | message | set by nick
+}
 
 ##
 ##	Custom sub added to make the getOutput more clear	
@@ -94,7 +209,7 @@ sub doRegexMatches{
 	## party party
 	if ($options=~/^everybody dance now/i){
 		$self->returnType("action");
-		return ("dances");
+		return ("twerks");
 	}
 
 	if ($options=~/^stop/i){
@@ -123,12 +238,10 @@ sub doRegexMatches{
 sub settings{
 	my $self = shift;
 
-	# Call defineSetting for as many settings as you'd like to define.
 	$self->defineSetting({
-		name=>'welcome_all',
-		default=>'yes',
-		allowed_values=>[qw(yes no)],
-		desc=>'Should '.$self->{BotName}.' welcome everyone to the room? Set to "yes" or "no".' 
+		name=>'herald_channels',
+		default=>'',
+		desc=>'A space separated list of channels that the herald should operate in.  If a channel is not listed here, the herald will not welcome users in that channel.'
 	});
 }
 
@@ -165,9 +278,9 @@ sub listeners{
 
 sub addHelp{
 	my $self = shift;
-	$self->addHelpItem("[plugin_description]", "Welcomes people to the room.");
-	$self->addHelpItem("herald", "Welcomes people to the room, or set a custom welcome message for a particular user. Flags: -nick=<nick> -channel=<#channel> -add=<message> -list -delete=<#>");
-	$self->addHelpItem("herald_settings", "Enable or disable herald options. -channel=<#channel>" );
+	$self->addHelpItem("[plugin_description]", "Welcomes people to the room. Set custom welcome messages for users.");
+	$self->addHelpItem("[herald]", "Welcomes people to the room, or set a custom welcome message for a particular user. Flags: -add -show -delete, with -nick=<nick> -channel=<#channel> -message=\"message\" -command -action -id=<#>");
+	$self->addHelpItem("[herald][-add]", "Add a herald message for a nick.  Use -command to indicate that the message should be executed as a command.  Use -action to indicate that the message should be announced as an action. (e.g. /me).  Example: herald -add -nick=\"SomeGuy\" -channel=\"#somechannel\"  -message=\"hello there!\" ");
 }
 1;
 __END__
