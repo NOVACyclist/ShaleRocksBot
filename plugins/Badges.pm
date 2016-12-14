c6c5e11eda5629067f4087c8ea37f5204ab4e974
NOVACyclist <NOVACyclist@users.noreply.github.com>
2016-12-14 09:26:25 -0500

    completed merge

--- plugins/Badges.pm
+++ plugins/Badges.pm
@@ -699,7 +699,6 @@ sub listeners {
 ## The help system will pull from here using PluginBaseClass->help(key).
 ##
 
-<<<<<<< .mine
 sub addHelp {
     my $self = shift;
     $self->addHelpItem( "[plugin_description]",
@@ -716,7 +715,7 @@ sub addHelp {
     $self->addHelpItem( "[badge][-delete]",
         "Usage: $self->{BotCommandPrefix}badge -delete -name=\"Badge Name\"" );
     $self->addHelpItem( "[badge][-cost]",
-"Set a daily cost for a badge. Example: $self->{BotCommandPrefix}badge -cost =(\$,\£, etc.)5.25 -name=\"Badge Name\". To clear a cost, use -cost = none"
+"Set a daily cost for a badge. Example: $self->{BotCommandPrefix}badge -cost =(\$, \£, kcal, A\$, C\$ etc.)5.25 -name=\"Badge Name\". To clear a cost, use -cost = none"
     );
     $self->addHelpItem( "[badge][-all]",
 "See system wide badges.  Usage: $self->{BotCommandPrefix}badge -all.  To see all badges of a particular type, use $self->{BotCommandPrefix}badge -all -name=\"Badge Name\""
@@ -727,19 +726,7 @@ sub addHelp {
     $self->addHelpItem( "[badge][-name]",
 "See a particular badge.  Usage: $self->{BotCommandPrefix}badge -name = \"Badge Name\""
     );
-=======
-sub addHelp{
-   my $self = shift;
-   $self->addHelpItem("[plugin_description]", "Badges. Get a date-based \"days since\" badge for whatever.");
-   $self->addHelpItem("[badge]", "Use $self->{BotCommandPrefix}badge to list your badges. Flags: -add -delete -update -cost, with -name=\"Badge Name\" -date=\"a date string\".  Use -all to see all users' badges, use -all -name=\"Badge Name\" to see all badges of a particular type for all users. Use -nick=SomeGuy to see SomeGuy's badges");
-   $self->addHelpItem("[badge][-add]", "Create a badge.  Usage: $self->{BotCommandPrefix}badge -add -name=\"Badge Name\" -date=\"some date\"");
-   $self->addHelpItem("[badge][-update]", "Change the date on a badge.  Usage: $self->{BotCommandPrefix}badge -update -name=\"Badge Name\" -date=\"some date\"");
-   $self->addHelpItem("[badge][-delete]", "Usage: $self->{BotCommandPrefix}badge -delete -name=\"Badge Name\"");
-   $self->addHelpItem("[badge][-cost]", "Set a daily cost for a badge. Example: $self->{BotCommandPrefix}badge -cost =(\$, \£, kcal, A\$, C\$ etc.)5.25 -name=\"Badge Name\". To clear a cost, use -cost = none");
-   $self->addHelpItem("[badge][-all]", "See system wide badges.  Usage: $self->{BotCommandPrefix}badge -all.  To see all badges of a particular type, use $self->{BotCommandPrefix}badge -all -name=\"Badge Name\"");
-   $self->addHelpItem("[badge][-nick]", "See another users badges.  Usage: $self->{BotCommandPrefix}badge -nick = someguy");
-   $self->addHelpItem("[badge][-name]", "See a particular badge.  Usage: $self->{BotCommandPrefix}badge -name = \"Badge Name\"");
->>>>>>> .r227
+    >> >> >> > . r227;
 }
 1;
 __END__
